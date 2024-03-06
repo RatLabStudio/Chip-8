afb2a5cf69b352e32691f96ec51f103e289aa3f5
Matteo <matteosalverio@gmail.com>
2024-03-06 12:38:21 -0500

    Chip8

---
+++ scripts/chip8.js
@@ -0,0 +1,38 @@
+//https://www.freecodecamp.org/news/creating-your-very-own-chip-8-emulator/
+
+import Renderer from "./renderer.js";
+import Keyboard from "./keyboard.js";
+import Speaker from "./speaker.js";
+import CPU from "./cpu.js";
+
+const renderer = new Renderer(10);
+const keyboard = new Keyboard();
+const speaker = new Speaker();
+const cpu = new CPU(renderer, keyboard, speaker);
+
+let loop;
+
+let fps = 60, fpsInterval, startTime, now, then, elapsed;
+
+function init() {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+
+    cpu.loadSpritesIntoMemory();
+    cpu.loadRom('TEST');
+    loop = requestAnimationFrame(step);
+}
+
+function step() {
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        cpu.cycle();
+    }
+
+    loop = requestAnimationFrame(step);
+}
+
+init();
